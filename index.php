b1ddf4483b44b759f814fb1ee3638b299214a62e
Irina Vasileva <Irina.Vasileva@technopark.ru>
2021-05-01 16:09:27 +0300

    module2-task1

--- index.php
+++ index.php
@@ -159,7 +159,7 @@ $products = [
         <ul class="nav__list container">
             <?php foreach ($categories as $category): ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?= $category ?></a>
+                    <a href="pages/all-lots.html"><?= $category['name'] ?></a>
                 </li>
             <?php endforeach; ?>
         </ul>
