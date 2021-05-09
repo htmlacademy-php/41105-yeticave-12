5b9f6844547c5fc071fb58dc3beb5ff6d69e637e
Irina Vasileva <Irina.Vasileva@technopark.ru>
2021-05-09 15:24:16 +0300

    module3-task1

--- templates/main.php
+++ templates/main.php
@@ -3,8 +3,8 @@
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
         <?php foreach ($categories as $category):?>
-            <li class="promo__item promo__item--<?= $category['code'] ?>">
-                <a class="promo__link" href="pages/all-lots.html"><?= $category['name'] ?></a>
+            <li class="promo__item promo__item--<?= htmlspecialchars($category['code']) ?>">
+                <a class="promo__link" href="pages/all-lots.html"><?= htmlspecialchars($category['name']) ?></a>
             </li>
         <?php endforeach;?>
     </ul>
@@ -20,12 +20,12 @@
                     <img src="<?= $product['url'] ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $product['category'] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $product['name'] ?></a></h3>
+                    <span class="lot__category"><?= htmlspecialchars($product['category']) ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($product['name']) ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $formatter->getPrice($product['price']) ?></span>
+                            <span class="lot__cost"><?= $formatter->getPrice(htmlspecialchars($product['price'])) ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
